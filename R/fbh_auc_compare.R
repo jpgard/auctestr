9c5a1b060bcd55bf63d6281c9758aec6d01b5416
Josh Gardner <joshua.patrick.gardner@gmail.com>
2017-11-07 07:35:45 -0500

    initial commit

---
+++ R/fbh_auc_compare.R
@@ -0,0 +1,86 @@
+library(dplyr)
+library(tidyr)
+
+
+## compute standard error of AUC score, using its equivalence to the Wilcoxon statistic (See Fogarty, Baker and Hudson, "Case Studies in the use of ROC Curve Analysis for Sensor-Based Estimates in Human Computer Interaction" 2008.)
+## auc: value of A' statistic.
+## n_p: number of positive cases.
+## n_n: number of negative cases.
+se_auc <- function(auc, n_p, n_n){
+    D_p = (n_p - 1)*( (auc/(2 - auc)) - auc^2)
+    D_n = (n_n - 1)*((2 * auc^2)/(1+auc) - auc^2) 
+    SE_auc = sqrt( (auc * (1-auc) + D_p + D_n) / (n_p * n_n) )
+    return(SE_auc)
+}
+
+## apply z-test for difference between auc_1 and auc_2 using FBH method.
+fbh_test <- function(auc_1, auc_2, n_p, n_n){
+    SE_auc_1 = se_auc(auc_1, n_p, n_n)
+    SE_auc_2 = se_auc(auc_2, n_p, n_n)
+    z = (auc_1 - auc_2)/sqrt(SE_auc_1^2 + SE_auc_2^2)
+    return(z)
+}
+
+## compute aggregate z-score using stouffer's method, ignoring NA values
+## z_vec: vector of z-scores.
+stouffer_z <- function(z_vec){
+    S_z = sum(z_vec, na.rm=TRUE)
+    k = sum(!is.na(z_vec))
+    stouffers_z = S_z/sqrt(k)
+    return(stouffers_z)
+}
+
+#' Apply the FBH method to compare \code{outcome_col} by \code{compare_col}, averaging over \code{time_col} (due to non-independence) and then over \code{over_col} by using Stouffer's Method.
+#' For more information, see Fogarty, Baker and Hudson, "Case Studies in the use of ROC Curve Analysis for Sensor-Based Estimates in Human Computer Interaction" 2008.
+#'
+#' @param df DataFrame containing \code{time_col}, \code{outcome_col}, \code{compare_col}, and \code{over_col}.
+#' @param compare_values names of models to compare (character vector of length 2). These should match exactly the names as they appear in compare_col.
+#' @param filter_value (optional) keep only observations which contain \code{filter_value} for \code{filter_col}.
+#' @param time_col name of column in df representing time of observations (z-scores are averaged over time_col within each model/course due to non-independence). These can also be other dependent groupings, such as cross-validation folds.
+#' @param outcome_col name of column in df representing outcome to compare; this should be Area Under the Receiver Operating Characteristic or A' statistic (this method applies specifically to AUC and not other metrics (i.e., sensitivity, precision, F1)..
+#' @param compare_col name of column in df representing two conditions to compare (should contain at least 2 unique values; these two values are specified as \code{compare_values}).
+#' @param over_col identifier for independent experiments, iterations, etc. over which z-scores for models are to be compared (using Stouffer's Z; see  Stouffer, S.A.; Suchman, E.A.; DeVinney, L.C.; Star, S.A.; Williams, R.M. Jr. (1949). The American Soldier, Vol.1: Adjustment during Army Life.)
+#' @param n_col name of column in df with total number of observations in the sample tested by each row.
+#' @param n_p_col name of column in df with n_p, number of positive observations.
+#' @param n_n_col name of column in df with n_n, number of negative observations.
+#' @param filter_col (optional) name of column in df to filter observations on; keep only observations which contain \code{filter_value} for \code{filter_col}.
+fbh_auc_compare <- function(df, compare_values, filter_value, time_col = "week", outcome_col = "auc", compare_col = "model", over_col = "course", n_col = "n", n_p_col = "n_p", n_n_col = "n_n", filter_col = "feat_type"){
+    ## TODO: check that time_col, outcome_col, compare_col, and over_col exist and are in names of dataframe.
+    ## TODO: check that len(compare_values) == 2
+    #TODO: add a default for compare_values; if not provided just take first two items alphabetically.
+    #TODO: make filter_col and filter_value optional.
+    filter_str = paste0(compare_col, " %in% c('", compare_values[1], "', '", compare_values[2], "') & ", filter_col, " == '", filter_value, "'")
+    comp_df = select(df, one_of(c(time_col, outcome_col, compare_col, over_col, n_col, n_p_col, n_n_col, filter_col))) %>% dplyr::filter_(filter_str)
+    n_courses = length(unique(comp_df[,over_col]))
+    course_z_scores = rep(NA, n_courses)
+    for (course_ix in seq_along(unique(comp_df[,over_col]))){
+        course = unique(comp_df[,over_col])[course_ix]
+        message(paste0("fetching comparison results for models ", compare_values[1], ", ", compare_values[2], " in course ", course, " with feature type ", filter_value))
+        course_df = comp_df[comp_df[,over_col] == course,]
+        n_weeks = length(unique(course_df[,time_col]))
+        course_week_z_scores = rep(NA, n_weeks)
+        for (week in unique(course_df[,time_col])){
+            course_week_df = course_df[course_df[,time_col] == week,]
+            # check whether both models have valid output for this week; otherwise skip
+            if(length(unique(course_week_df[,compare_col])) != length(compare_values)){
+                msg = paste0("Missing performance data for at least one model in course ", course, " week ", week, "; skipping")
+                message(msg)
+                next
+            }
+            auc.1 = course_week_df[course_week_df[,compare_col] == compare_values[1], outcome_col]
+            auc.2 = course_week_df[course_week_df[,compare_col] == compare_values[2], outcome_col]
+            n.p = course_week_df[1,n_p_col]
+            n.n = course_week_df[1,n_n_col]
+            # conduct test and store z score in course_week_z_scores
+            z = fbh_test(auc_1 = auc.1, auc_2 = auc.2, n_p = n.p, n_n = n.n)
+            course_week_z_scores[week] <- z
+        }
+        # calculate average z-score across weeks and store
+        course_z = mean(course_week_z_scores, na.rm = TRUE)
+        course_z_scores[course_ix] <- course_z 
+    }
+    # applying stouffer's Z method to course_z_scores
+    overall_z = stouffer_z(course_z_scores)
+    return(overall_z)
+}
+
